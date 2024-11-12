db05330efe165573210cca12c26be53ae5396858
katxf <157767063+katxf@users.noreply.github.com>
2024-11-12 16:09:47 -0500

    Update skincare.py
    A basic layout of the code and what needs to be done for check-in 1

--- skincare.py
+++ skincare.py
@@ -1,1 +1,19 @@
+"""
+Group: Katherine Fuentes, Krista Mathew, Jigyasa Dahal, Afreen Ahmed
+Assignment: Final Project Check in - Skincare
+Date: 11/12/24
+"""
 
+##Layout of the code/Docustrings
+
+def skin_profile(): ## creates a profile for user which stores their own skin data
+
+def skincare_products(): ## stores the full list of skincare products 
+
+def skincare_recs(): ## skincare recommendations based on user's skin from products they have
+
+def skincare_sug(): ## suggests additional products that the user does not already own 
+
+## Unit Tests 
+
+assert == 
