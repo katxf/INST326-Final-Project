ce8a4506b2cf9388ab7e6cb34811ed1f7bf53d31
kmathew3 <krista.a.mathew@gmail.com>
2024-11-26 21:05:08 -0500

    Update skincare.py
    added if name main

--- skincare.py
+++ skincare.py
@@ -4,7 +4,7 @@ Assignment: Final Project Check in - Skincare
 Date: 11/26/24
 """
 
-##Layout of the code/Docustrings
+##Layout of the code/Docstrings
 class User:
   """A class for the user's skin profile including skin type, goals, and their daily log"""
   def __init__(self, skin_type, goals):
@@ -33,17 +33,57 @@ class Skincare_Product:
   def skincare_products(self):
     """Returns the user's skincare product information"""
     return {"name": self.name, "skin_type": self.skin_type, "target": self.target, "owned": self.owned}
+
+
 class Skincare_Advice:
   """A class that recommends skincare products based on their current inventory and skin type. 
   As well as new products that the user does not own"""
-def skincare_recs(): 
-  """Skincare recommendations based on user's skin from products they have"""
-  self.skin_type = skin_type
-  self.owned = owned 
-  self.recs = {skin_type:owned}
-def skincare_sug(): 
-  """Suggests additional products that the user does not already own"""
-  self.newProducts = []
-  self.skin_type = skin_type 
-  self.new_routine = new_routine 
+  def __init__(self, user, products):
+    """Sets the user's profile and their current products"""
+    self.user = user
+    self.products = products
+    
+  def skincare_recs(self, skin_type, owned, recs): 
+    """Skincare recommendations based on user's skin from products they have"""
+    self.skin_type = skin_type
+    self.owned = owned 
+    self.recs = {skin_type:owned}
+    
+  def skincare_sug(self, newProducts, skin_type, new_routine):
+    """Suggests additional products that the user does not already own"""
+    self.newProducts = []
+    self.skin_type = skin_type 
+    self.new_routine = new_routine 
   
+## Unit Tests and Example Usage
+"""Asserts the four main skin types and equates it to the corresponding object. """
+
+assert self.skin_type == "combo skin" 
+assert self.skin_type == "oily skin" 
+assert self.skin_type == "normal skin" 
+assert self.skin_type == "dry skin"
+assert advice.skincare_recs() == ["Oil-Free Moisturizer", "SPF 50 Sunscreen"]
+assert advice.skincare_sug(possible_products) == ["Toner"]
+assert user.skin_profile()["user_log"]["2024-11-20"]["condition"] == "oily skin"
+
+if __name__ == "__main__":
+  user = User("oily skin", ["hydration", "acne control"])
+  user.log("2024-11-20", "oily skin", "skin is oily with slight texture")
+  user.log("2024-11-21", "combo skin", "skin has improved but texture remains")
+  
+  moisturizer = Skincare_Product("Oil-Free Moisturizer", "oily skin", "hydration")
+  cleanser = Skincare_Product("Foaming Facial Cleanser", "oily skin", "hydration")
+  sunscreen = Skincare_Product("SPF 50 Sunscreen", "combo skin", "UV protection")
+  user_products = [moisturizer, cleanser, sunscreen]
+
+  possible_products = [
+    Skincare_Product("Hydrating Serum", "dry skin", "hydration", owned = False),
+    Skincare_Product("Toner", "oily skin", "acne control", owned = False),
+    Skincare_Product("Oil-Free Moisturizer", "oily skin", "hydration", owned = True),
+    Skincare_Product("SPF sunscreen", "combo", "UV protection", owned = False)
+  ]
+
+  advice = Skincare_Advice(user, user_products)
+
+  print(f"Recommended products that you own: {advice.skincare_sug(skincare_recs()}")
+  print(f"\nProducts you might want to purchase: {adivce.skincare_sug(possible_products)}")
